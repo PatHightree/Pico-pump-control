d0e376df87cdece66ff32c373f9afe8d921711b7
Patrick Hogenboom <patrick.hogenboom@pm.me>
2023-07-07 01:03:53 +0200

    No auto-start, restartable

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from web_server import *
 from pump_control import *
 
+led.off()
 start_timers()
 try:
     ip = connect()
     connection = open_socket(ip)
+    led.on()
     serve(connection)
 except KeyboardInterrupt:
+    led.off()
+    connection.close()
+    print('Connection was closed')
     machine.reset()
